c7949be0827780ddec32d7dc43d568c80f7695e1
sergeykotenko <sergeykotenko@bk.ru>
2022-10-19 01:13:32 +0300

    favicon and data

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 new Swiper('.slider')
+
+const date = new Date()
+console.log(date)
+let futureDate = new Date()
+if (date.getDay()) {
+	futureDate.setDate(date.getDate() + 8 - date.getDay())
+} else {
+	futureDate.setDate(date.getDate() + 1)
+}
+const dateNeedFormat = new Intl.DateTimeFormat().format(futureDate)
+
+const sale = document.getElementById('sale')
+sale.textContent = dateNeedFormat
